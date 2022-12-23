e168e94820193be49cba03675ddb5b0bbfc39d08
Norbert305 <sealsnorbert@yahoo.com>
2022-12-23 23:10:34 +0000

    update2

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -1,20 +1,23 @@
 people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
 #Your code go here:
+people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
+
+#Your code go here:
 def deletePerson(person_name):
-    
-    x = []
     #Your code go here:
-    for name in person_name:
-        
-        if person_name == "daniella" :
-            x=['juan','ana','michelle','stefany','lucy','barak']
-        elif person_name == "juan":
-            x=['ana','michelle','daniella','stefany','lucy','barak']
-        else:
-            x=['juan','ana','michelle','daniella','stefany','lucy','barak']
-        
-    return x
+
+    answer = []
+
+    for i in range(len(people)):
+        if people[i] != person_name:
+            answer.append(people[i])
+
+    return answer
+
+    
+
+
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
 print(deletePerson("emilio"))
